925183043fe3ef88f70b6232e220ab2a235f7baf
unknown <913637919@qq.com>
2023-10-31 20:00:20 +0800

    新增Http工具类，用户注册时自动调用post请求在im系统中新增对应用户

--- src/main/java/com/gker/gkerlove/controller/UserController.java
+++ src/main/java/com/gker/gkerlove/controller/UserController.java
@@ -12,8 +12,10 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.annotation.Resource;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.web.bind.annotation.*;
+import com.tencentyun.TLSSigAPIv2;
 
-
+import com.alibaba.fastjson.JSONObject;
+import com.gker.gkerlove.util.HttpUtils;
 @RestController
 @Tag(name = "用户相关接口")
 @RequestMapping("/user")
@@ -42,6 +44,15 @@ public class UserController {
         }
         httpSession.removeAttribute("captcha");
         userService.register(user, code);
+        //在im中也注册一个用户
+        TLSSigAPIv2 api = new TLSSigAPIv2(1600009914, "d82b484df8d55fc6077400a56a4a42a5ef7ce2a53ee05777e7b416f6ee6d0c79");
+        String Sig = api.genUserSig("administrator", 60);
+        String url = "https://console.tim.qq.com/v4/im_open_login_svc/account_import?sdkappid=1600009914&identifier=administrator&usersig="+Sig+"&random=99999999&contenttype=json";
+        JSONObject param = new JSONObject();
+        param.put("UserID",user.getUsername());
+        param.put("Nick",user.getUsername());
+        param.put("FaceUrl","");
+        String s = HttpUtils.sendPost(url, param.toJSONString());
         return R.ok().message("注册成功");
     }
 
@@ -53,4 +64,6 @@ public class UserController {
     }
 
 
+
 }
+
