852575c840237d4e25183e783192a10dfd369ff0
gyr <157679566@qq.com>
2023-11-11 15:44:44 +0800

    feature: 添加获取OSS临时操作凭证接口

---
+++ src/main/java/com/gker/gkerlove/service/OSSService.java
@@ -0,0 +1,34 @@
+package com.gker.gkerlove.service;
+
+import com.aliyuncs.DefaultAcsClient;
+import com.aliyuncs.auth.sts.AssumeRoleRequest;
+import com.aliyuncs.auth.sts.AssumeRoleResponse;
+import com.aliyuncs.exceptions.ClientException;
+import com.aliyuncs.profile.DefaultProfile;
+import com.aliyuncs.profile.IClientProfile;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class OSSService {
+    @Value("${OSS.accessKeyId}")
+    private String accessKeyId;
+    @Value("${OSS.accessKeySecret}")
+    private String accessKeySecret;
+    @Value("${OSS.roleArn}")
+    private String roleArn;
+
+    public AssumeRoleResponse assumeRole(String roleSessionName) throws ClientException {
+        IClientProfile profile = DefaultProfile.getProfile("cn-hangzhou", accessKeyId, accessKeySecret);
+        DefaultAcsClient client = new DefaultAcsClient(profile);
+
+        AssumeRoleRequest request = new AssumeRoleRequest();
+        request.setRoleArn(roleArn);
+        request.setRoleSessionName(roleSessionName);
+        request.setDurationSeconds(3600L);
+
+        return client.getAcsResponse(request);
+    }
+
+}
