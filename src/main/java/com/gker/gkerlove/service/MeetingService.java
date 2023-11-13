82726b8663634b85041cd433752ff7a53d60f52f
Yuichi <913637919@qq.com>
2023-11-13 19:21:40 +0800

    添加好友的时候自动修改自己和对方的喜欢值和人气值

--- src/main/java/com/gker/gkerlove/service/MeetingService.java
+++ src/main/java/com/gker/gkerlove/service/MeetingService.java
@@ -2,8 +2,14 @@ package com.gker.gkerlove.service;
 
 
 import com.alibaba.fastjson.JSONObject;
+import com.gker.gkerlove.bean.User;
 import com.gker.gkerlove.util.HttpUtils;
 import com.tencentyun.TLSSigAPIv2;
+import jakarta.annotation.Resource;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -11,6 +17,8 @@ import java.util.List;
 
 @Service
 public class MeetingService {
+    @Resource
+    MongoTemplate mongoTemplate;
     public String MeetingAddlover(String formusername,String tousername) {
         TLSSigAPIv2 api = new TLSSigAPIv2(1600009914, "d82b484df8d55fc6077400a56a4a42a5ef7ce2a53ee05777e7b416f6ee6d0c79");
         String Sig = api.genUserSig("administrator", 60);
@@ -26,6 +34,30 @@ public class MeetingService {
         frienditem.put("AddWording","我将你列为了我喜欢的人，如果对我的个人信息感兴趣的话，就将我添加为好友开始聊天吧！");
         frienditems.add(frienditem);
         param.put("AddFriendItem",frienditems);
+
+        Query query = new Query(Criteria.where("username").is(tousername));
+        User touser = mongoTemplate.findOne(query, User.class);
+        Integer liked_num = null;
+        if (touser != null) {
+            liked_num = touser.getLikedBy() + 1;
+        }
+        else{
+            liked_num = 1;
+        }
+        Update update = Update.update("liked_by",liked_num);
+        mongoTemplate.updateFirst(query,update, "users");
+
+        Query query1 = new Query(Criteria.where("username").is(formusername));
+        User fromuser = mongoTemplate.findOne(query, User.class);
+        Integer like_num = null;
+        if (fromuser != null) {
+            like_num = fromuser.getLikes() + 1;
+        }
+        else{
+            like_num = 1;
+        }
+        Update update1 = Update.update("likes",like_num);
+        mongoTemplate.updateFirst(query1,update1, "users");
         return HttpUtils.sendPost(url, param.toJSONString());
     }
 }
